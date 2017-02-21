1902499d375932c847d63d3d9cb74f41f8e25ec5
Mirko Fetter <mirko.fetter@uni-bamberg.de>
2017-02-21 18:36:47 +0100

    Stubs for TuioBlob, TuioCursor and TuioObject

---
+++ TUIOSwift/TuioBlob.swift
@@ -0,0 +1,13 @@
+//
+//  TuioBlob.swift
+//  TUIOSwift
+//
+//  Created by Mirko Fetter on 21.02.17.
+//  Copyright © 2017 grugru. All rights reserved.
+//
+
+import Foundation
+
+class TuioBlob{
+    
+}
---
+++ TUIOSwift/TuioCursor.swift
@@ -0,0 +1,14 @@
+//
+//  TuioCursor.swift
+//  TUIOSwift
+//
+//  Created by Mirko Fetter on 21.02.17.
+//  Copyright © 2017 grugru. All rights reserved.
+//
+
+import Foundation
+
+
+class TuioCursor{
+    
+}
